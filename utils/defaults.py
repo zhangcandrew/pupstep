1ad5edc87b66a68910464acd259b9a49c6b606d7
zhangcandrew <zhangcandrew@gmail.com>
2019-10-27 18:43:34 -0400

    folder structure

--- utils/defaults.py
+++ utils/defaults.py
@@ -3,5 +3,5 @@ import os
 DOWNLOAD_PATH = '/home/andy/GitProjects/pupstep/dogsounds/unprocessed'
 API_KEY=str(open('API_KEY.txt', 'r').read())
 VIDEO_FILE_PATHS = [[os.path.join(root, name) for name in files] for root, _, files in os.walk(DOWNLOAD_PATH)]
-PROCESSED_VIDEOS_PATH = '/home/andy/GitProjects/pupstep/dogsounds/processed/'
-SOUND_FILES_PATH = '/home/andy/GitProjects/pupstep/dogsounds/soundfiles/'
+PROCESSED_VIDEOS_PATH = '/home/andy/GitProjects/pupstep/dogsounds/processed/wavvideos'
+SOUND_FILES_PATH = '/home/andy/GitProjects/pupstep/dogsounds/processed/soundfiles/'
